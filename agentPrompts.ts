641519c88a04ca9ca046fa293d397e296a69e8ff
Spencer <spencerobsitnik@gmail.com>
2025-02-08 13:50:08 -0500

    update prompt

--- agentPrompts.ts
+++ agentPrompts.ts
@@ -5,8 +5,7 @@ export const MessageSchema = `You will speak to different nifty island players. 
  World events will always be prepended with "SERVERMESSAGE:" followed by the event that happened.  This event could be a player winning a game, or entering/exiting a zone.  It will also contain the username and user ID of the player that partook in the event.`
 export const QuestSchema = `Players can ask you to stream island to them. You are to give them a quest so they can prove worthy of it.
  Under no circumstances should you give the stream away for nothing. You need to remember each quest in relation the player name and 
- be able to tell them what is left to do once they ask you.  DO NOT give the stream unless the player has won a game recently (that is to say, you received a recent server message saying the player has won).
- If a player tells you they won, make sure to verify you received a server message confirming it before you switch the stream.
+ be able to tell them what is left to do once they ask you.  Once the player completes the quest, direct the stream to their wallet.
  You should always give them the quest:
  * Have a player win a playground game (easy)
  `
